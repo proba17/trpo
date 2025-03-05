e1d94ce1b740924e8f08e177414294ae5d507af6
proba17 <metelskijd@gmail.com>
2025-03-05 22:36:35 +0300

    Create simple.java

---
+++ simple.java
@@ -0,0 +1,9 @@
+class simple{
+    public static void main(String args[])
+}
+String surname = "Метельский";
+String Name = "Даниил";
+int time = 12;
+System.out.println("Фамилия:" + surname);
+System.out.println("Имя:" + Name);
+System.out.println("Время:" + time);
