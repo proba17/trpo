e747e1ea5e21c113a0df86c1b2e66cab2e32781d
proba17 <151502793+proba17@users.noreply.github.com>
2025-03-23 21:36:48 +0300

    Update simple.java

--- simple.java
+++ simple.java
@@ -1,9 +1,11 @@
-class simple{
-    public static void main(String args[])
+public class Simple {
+    public static void main(String[] args) { 
+        String surname = "Metelskiy";
+        String name = "Daniil";
+        int time = 12;
+
+        System.out.println("Surname: " + surname);
+        System.out.println("Name: " + name);
+        System.out.println("Time: " + time);
+    }
 }
-String surname = "Metelskiy";
-String Name = "Daniil";
-int time = 12;
-System.out.println("Surname:" + surname);
-System.out.println("Name:" + Name);
-System.out.println("Time:" + time);
